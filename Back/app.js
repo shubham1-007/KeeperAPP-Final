378f851434eb5050fa9a840131a9b4c12ad2d252
shubham <khaiers750@gmail.com>
2022-12-29 11:03:44 +0530

    first commit

---
+++ Back/app.js
@@ -0,0 +1,68 @@
+const express=require('express');
+const app=express();
+const mongoose=require('mongoose');
+const { response } = require("express");
+const bodyParser=require('body-parser')
+const cors=require("cors");
+const exp = require('constants');
+const path = require('path');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(path.resolve(__dirname,'../Front/build')))
+app.set('view engine','ejs')
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+app.use(cors(corsOptions)) // Use this after the variable declaration
+mongoose.connect('mongodb+srv://AfterReact:AfterReact@cluster0.dllf1mj.mongodb.net/?retryWrites=true&w=majority');
+const NoteSchema=mongoose.Schema({
+    title:String,
+    content:String
+})
+const Note=mongoose.model("Note",NoteSchema);
+const DefNote=new Note({
+    title:"this is the title",
+    content:"This is the content"
+})
+// DefNote.save();
+
+app.get("/api",function(req,res){
+  
+   Note.find(function(err,foundItems){
+
+    console.log(foundItems)
+    //    foundItems.map(item=>{
+       
+    //  console.log(item.name)
+     
+     
+    // });
+
+    // res.render("index",{newItems:foundItems})
+    res.send(foundItems)
+    })
+});
+   
+// app.post("/api",function(req,res){
+//     console.log("req rec");
+//     // res.send("request received"+req.body.Item)
+//     const newNote=new Note({
+//         title:req.body.title,
+//         content:req.body.content
+//     })
+//     newNote.save();
+//     res.redirect("/api")
+// })
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"../Front/build",'index.html'))
+})
+
+
+   
+const port= process.env.port || 7000
+app.listen(port,function(){
+
+    console.log("server is running and up my darling 💕 the port"+port);
+})
---
+++ Front/src/components/CreateArea.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import AddIcon from "@material-ui/icons/NoteAdd";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+  const [isExpanded, setExpanded] = useState(false);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    var input=(event.target.value);
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+
+    fetch('http://localhost:4000/api' , {
+      method : 'POST',
+  
+      body : JSON.stringify(name),
+  
+      headers : {
+          'Content-type' : 'application/json'
+      }
+  });
+
+
+
+
+
+    
+  }
+
+
+
+
+  
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
+  function expansion() {
+    setExpanded((prev) => {
+      return !prev;
+    });
+  }
+
+  return (
+    <div>
+      <form className="create-note">
+        {isExpanded && (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
+        <textarea
+          name="content"
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={isExpanded ? "3" : 1}
+          onClick={expansion}
+        />
+        <Zoom in={isExpanded}>
+          <Fab onClick={submitNote}>
+            <AddIcon></AddIcon>
+          </Fab>
+        </Zoom>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
